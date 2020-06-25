9cf15b7c3ce6ab99f8dc69a66faa6064e84399cf
Guilherme <parra.guioliveira@hotmail.com>
2020-06-25 15:21:27 -0300

    Finalizado

---
+++ Fibonacci.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Aula25
+{
+    public class Fibonacci
+    {
+        public void GerarSequenciaFibonacci(int num1, int num2, int num3, int parada){
+            //condição crucial de parada da recursividade
+            if(parada>=0){
+                
+                Console.WriteLine(num1);
+                
+                GerarSequenciaFibonacci(num2, num3, num1 + num3 + num2, parada - 1 );
+            }
+            
+            }
+            public int GerarFatorial(int numero){
+                if(numero == 1){
+                    return 1;
+                }else{
+                    return numero * GerarFatorial(numero-1);
+                }
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci rec = new Fibonacci();
+            rec.GerarSequenciaFibonacci(0,1,1,15);
+            Console.WriteLine(rec.GerarFatorial(5));
+        }
+    }
+}
